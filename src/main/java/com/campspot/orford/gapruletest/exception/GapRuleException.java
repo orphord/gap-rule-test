288b99e80bc5156a612ddaa51b908eeb45c27f19
Jeff Orford <orphord@gmail.com>
2018-09-28 16:59:38 -0600

    An exception class to be a catch-all for the various errors that might occur.

---
+++ src/main/java/com/campspot/orford/gapruletest/exception/GapRuleException.java
@@ -0,0 +1,8 @@
+package com.campspot.orford.gapruletest.exception;
+
+public class GapRuleException extends Exception {
+	public GapRuleException() {}
+	public GapRuleException(String _msg) {
+		super(_msg);
+	}
+}

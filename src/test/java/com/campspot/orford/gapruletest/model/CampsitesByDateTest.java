08d592dfb8bee236f85dd70dcdf33b0b9c5e4327
Jeff Orford <orphord@gmail.com>
2018-10-03 16:08:28 -0600

    initial commit of some controller and model test code.

---
+++ src/test/java/com/campspot/orford/gapruletest/model/CampsitesByDateTest.java
@@ -0,0 +1,94 @@
+package com.campspot.orford.gapruletest.model;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+
+import com.campspot.orford.gapruletest.service.CampsiteService;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class CampsitesByDateTest {
+	private static final Logger log = LoggerFactory.getLogger(CampsitesByDateTest.class);
+
+	List<Reservation> reservations = new ArrayList<Reservation>();
+	Reservation testReservationEarly = new Reservation();
+	Reservation testReservationLate  = new Reservation();
+	Reservation testReservationOverlap = new Reservation();
+	
+	CampsitesByDate testCbd = new CampsitesByDate();
+
+	@Before
+	public void setUp() throws Exception {
+		CampsiteService csSvcMock = mock(CampsiteService.class);
+		Set<Integer> campsiteIDs = new HashSet<Integer>();
+		campsiteIDs.add(new Integer(1));
+		campsiteIDs.add(new Integer(2));
+		campsiteIDs.add(new Integer(3));
+		when(csSvcMock.getCampsiteIDs()).thenReturn(campsiteIDs);
+		
+		testReservationEarly.setCampsiteID(new Integer(1));
+		testReservationEarly.setStartDate(LocalDate.parse("2018-06-01"));
+		testReservationEarly.setEndDate(LocalDate.parse("2018-06-07"));
+		testReservationLate.setCampsiteID(new Integer(1));
+		testReservationLate.setStartDate(LocalDate.parse("2018-06-10"));
+		testReservationLate.setEndDate(LocalDate.parse("2018-06-17"));
+		testReservationOverlap.setCampsiteID(new Integer(1));
+		testReservationOverlap.setStartDate(LocalDate.parse("2018-06-05"));
+		testReservationOverlap.setEndDate(LocalDate.parse("2018-06-12"));
+		reservations.add(testReservationEarly);
+		reservations.add(testReservationLate);
+		reservations.add(testReservationOverlap);
+		
+		// Setup test CampsitesByDate object
+		LocalDate startDate = LocalDate.parse("2016-04-01");
+		int gapDays = 1; // Assuming 1 gap day for testing purposes
+		LocalDate latestUnaccept = testReservationLate.getEndDate().plusDays(gapDays + 1);
+		List<LocalDate> dates = this.datesBetweenDates(startDate, latestUnaccept);
+		testCbd.initialize(dates, csSvcMock.getCampsiteIDs());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAddReservationReservedDates() {
+		List<LocalDate> resDates =  this.datesBetweenDates(testReservationEarly.getStartDate(), testReservationEarly.getEndDate());
+		testCbd.removeCampsiteByDates(testReservationEarly, resDates);
+		LocalDate testDate = testReservationEarly.getStartDate().plusDays(1);
+		Set<Integer> remainingSiteIDs = testCbd.get(testDate);
+
+		assertThat(remainingSiteIDs, not(contains(new Integer(1))));
+		assertThat(remainingSiteIDs, contains(2,3));
+
+	}
+
+	
+	private List<LocalDate> datesBetweenDates(LocalDate _start, LocalDate _end) {
+		List<LocalDate> daysBetween = new ArrayList<LocalDate>();
+		long numDaysBetween = ChronoUnit.DAYS.between(_start, _end);
+		for(int i = 0; i <= numDaysBetween; i++) {
+			daysBetween.add(_start.plusDays(i));
+		}
+
+		return daysBetween;
+	}
+
+}
---
+++ src/test/java/com/campspot/orford/gapruletest/model/ReservationTest.java
@@ -0,0 +1,54 @@
+package com.campspot.orford.gapruletest.model;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalDate;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ReservationTest {
+
+	private Reservation testReservation;
+
+	@Before
+	public void setUp() throws Exception {
+		testReservation = new Reservation();
+		testReservation.setCampsiteID(new Integer(1));
+		testReservation.setStartDate(LocalDate.parse("2018-06-01"));
+		testReservation.setEndDate(LocalDate.parse("2018-06-07"));
+
+	}
+
+	@Test
+	public void testCampsiteID() {
+		Integer expectedID = new Integer(1);
+		assertEquals(testReservation.getCampsiteID(), expectedID);
+	}
+
+	
+	@Test
+	public void testStartDate() {
+		LocalDate expectedStartDate = LocalDate.parse("2018-06-01");
+		assertEquals(testReservation.getStartDate(), expectedStartDate);
+	}
+
+	@Test 
+	public void testEndDate() {
+		LocalDate expectedEndDate = LocalDate.parse("2018-06-07");
+		assertEquals(testReservation.getEndDate(), expectedEndDate);
+	}
+	
+	@Test
+	public void testFailStartDate() {
+		LocalDate unExpectedStart = LocalDate.parse("2018-01-22");
+		assertNotEquals(testReservation.getStartDate(), unExpectedStart);
+	}
+
+	@Test
+	public void testFailEndDate() {
+		LocalDate expectedEnd = LocalDate.parse("2018-01-25");
+		assertNotEquals(testReservation.getEndDate(), expectedEnd);
+	}
+}
